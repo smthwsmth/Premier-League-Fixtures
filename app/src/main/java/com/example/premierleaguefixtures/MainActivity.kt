bbbece36e167026dceea4e303b0c0f4d395276e9
Denis Trofimliuk <smthwsmth@gmail.com>
2024-07-02 21:22:20 +0300

    All irrelevant activities and scratches were deleted

--- app/src/main/java/com/example/premierleaguefixtures/MainActivity.kt
+++ app/src/main/java/com/example/premierleaguefixtures/MainActivity.kt
@@ -1,23 +1,31 @@
 package com.example.premierleaguefixtures
 
+
 import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
     }
+    fun toSchedule(view: View) {
+        val transfer = Intent(this, ScheduleActivity::class.java)
+        startActivity(transfer)
+        }
 
-    fun goToMenu(view: View) {
-        val transfer = Intent(this, SecondActivity::class.java)
+
+    fun toRating(view: View) {
+        val transfer = Intent(this, RatingActivity::class.java)
         startActivity(transfer)
     }
+
+
+
 }
-
-
--- app/src/main/java/com/example/premierleaguefixtures/ScheduleActivity.kt
+++ app/src/main/java/com/example/premierleaguefixtures/ScheduleActivity.kt
@@ -1,6 +1,9 @@
 package com.example.premierleaguefixtures
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,12 +12,29 @@ import androidx.core.view.WindowInsetsCompat
 class ScheduleActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_schedule)
 
 
         val actionBar = supportActionBar
 
         actionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+    // function to display all buttons on ActionBar from "menu---> main.xml"
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main, menu)         // inflate the ActionBar with buttons
+        return true
+    }
+
+    // function to react on ActionBar's buttons pushes
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // checks if button "home" was pressed. If true - go back to previous page
+        when(item.itemId) {
+            android.R.id.home -> finish()
+            R.id.dropdown_menu -> {
+                Toast.makeText(this, "Dropdown Menu", Toast.LENGTH_LONG).show()
+            }
+        }
+        return true
+
     }
 }
