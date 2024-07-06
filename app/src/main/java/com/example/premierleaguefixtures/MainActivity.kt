8dbd00cf487a724c875010fdeab67a4505e88257
Denis Trofimliuk <smthwsmth@gmail.com>
2024-07-06 21:18:52 +0300

    H/W#2 AppBar was added, Main page was created

--- app/src/main/java/com/example/premierleaguefixtures/MainActivity.kt
+++ app/src/main/java/com/example/premierleaguefixtures/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.premierleaguefixtures
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,6 +13,27 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val actionBar = supportActionBar
+
+        actionBar?.setDisplayHomeAsUpEnabled(true)
+
+    }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main, menu)         // inflate the ActionBar with buttons
+        return true
+    }
+
+    // function to react on ActionBar's buttons pushes
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // checks if button "home" was pressed. If true - go back to previous page
+        when(item.itemId) {
+            android.R.id.home -> finish()
+            R.id.dropdown_menu -> {
+                Toast.makeText(this, "Dropdown Menu", Toast.LENGTH_LONG).show()
+            }
+        }
+        return true
 
     }
 }
