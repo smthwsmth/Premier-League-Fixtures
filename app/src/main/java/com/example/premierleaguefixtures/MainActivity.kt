797f11d9046e03acd95691a61d5a5230f581dc20
smthwsmth <119123822+smthwsmth@users.noreply.github.com>
2024-07-07 00:11:27 +0300

    Revert "Homework#2 realizing part2"

--- app/src/main/java/com/example/premierleaguefixtures/MainActivity.kt
+++ app/src/main/java/com/example/premierleaguefixtures/MainActivity.kt
@@ -1,22 +1,20 @@
 package com.example.premierleaguefixtures
 
-import android.content.Intent
 import android.os.Bundle
-import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-    }
-    fun goToMenu(view: View) {
-        val transfer = Intent(this, SecondActivity::class.java)
-        startActivity(transfer)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
     }
 }
-
-
